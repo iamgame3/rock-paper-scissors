916849518a6fdb8ef6a94ffb7dddc1a4b7cc0279
iamgame3 <kosmosworld9@hotmail.com>
2022-11-18 19:40:30 -0500

    Properly link js and add computer choice function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 /*round starts*/
 /*computer makes choice*/
+let computerChoice = () => {
+    let randomChoice = Math.floor(Math.random() * 100);
+    if (randomChoice <=33) {
+        return "rock"
+    }  else if (randomChoice <=66) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
 /*human makes choice*/
 /*it is decided who won*/
 /*if rounds has reached 5, final winner is decided, else, game continues*/
