4ccb7909380883e960335dcdef618c914d36791b
iamgame3 <kosmosworld9@hotmail.com>
2022-11-19 19:27:20 -0500

    Add fiveRounds and game functions, and edit other functions

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-/*round starts*/
 let round = (a, b) => {
     if (a == "rock" && b == "scissors") {
         console.log("You lose! The computer chose rock!");
+        return "comp win";
     }   else if (a == "scissors" && b == "paper") {
         console.log("You lose! The computer chose scissors!");
+        return "comp win"
     }   else if (a == "paper" && b == "rock") {
         console.log("You lose! The computer chose paper!");
+        return "comp win";
     }   else if (a == "scissors" && b == "rock") {
         console.log("You win! The computer chose scissors!");
+        return "human win";
     }   else if (a == "rock" && b == "paper") {
         console.log("You win! The computer chose rock!");
+        return "human win";
     }   else if (a == "paper" && b == "scissors") {
         console.log("You win! The computer chose paper!");
+        return "human win";
     }   else {
         console.log("It was a tie! Try again!");
     }
 }
-/*computer makes choice*/
+
 let computerChoice = () => {
     let randomChoice = Math.floor(Math.random() * 100);
     if (randomChoice <=33) {
@@ -27,7 +32,7 @@ let computerChoice = () => {
         return "scissors"
     }
 }
-/*human makes choice*/
+
 let humanChoice = () => {
     humanInput = prompt("Rock, paper, or scissors?");
     correctedHumanInput = humanInput.toLowerCase();
@@ -37,5 +42,34 @@ let humanChoice = () => {
         humanChoice();
 }
 }
-/*it is decided who won*/
-/*if rounds has reached 5, final winner is decided, else, game continues*/
+
+let fiveRounds = () => {
+    let humanScoreFinal = 0;
+    let computerScoreFinal = 0;
+    for (let i = 0; i < 5; i++) {
+        a = computerChoice();
+        b = humanChoice();
+        point = round(a, b);
+        if (point == "human win") {
+            humanScoreFinal++;
+        }   else if (point == "comp win") {
+            computerScoreFinal++;
+        }
+    }
+    if (humanScoreFinal > computerScoreFinal) {
+        return "human win";
+    }   else if (computerScoreFinal > humanScoreFinal) {
+        return "comp win";
+    }
+}
+
+let game = () => {
+    finalScore = fiveRounds();
+    if (finalScore == "comp win") {
+        return ("You suck! The computer beat you!");
+    }   else if (finalScore == "human win") {
+        return ("You rule! You beat the computer!");
+    }   else {
+        return ("How interesting... It was a tie.");
+    }
+}
